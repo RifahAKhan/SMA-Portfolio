c3036a4f4db04f78cfbfb9237cf9b74c8d514be4
Rifah A Khan <rifahakhan@gmail.com>
2024-12-07 19:46:31 +0530

    Commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*typing animation*/
+var typed = new Typed(".typing",{
+    strings: ["","Visual Artist", "Photographer","Cinematographer","Cinematic Mentor","DI-Colorist","Editor"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
